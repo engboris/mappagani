a3004aad3ffdac79c3b550f97b11bc44d61dfbbd
engboris <engboris@hotmail.fr>
2016-11-16 18:11:52 +0100

    Progression.

--- color_solver.ml
+++ color_solver.ml
@@ -28,22 +28,53 @@ let list_to_indices (ls : 'a list) : int list =
   | _::t -> i :: (aux t (i+1))
   in aux ls 0;;
 
+(* ----------- Existence ----------- *)
 let clause_existence seeds : Logic.literal list list = 
-  let aux i = List.map (fun c -> (true, (i, c))) colors_set
+  let aux i = (List.map (fun c -> (true, (i, c))) colors_set)
   in (List.map (fun i -> aux i) (list_to_indices seeds));;
 
-let clausal_or_distribution c (cs : Logic.literal list) : Logic.literal list list =
+(* ----------- Unicity ----------- *)
+let clausal_distribution c (cs : Logic.literal list) : Logic.literal list list =
   List.map (fun x -> c::[x]) cs;;
  
 let clause_unicity seeds : Logic.literal list list =
-  let phi i c =
+  let psi i c =
     let other_colors = List.filter (fun x -> x <> c) colors_set in
-    clausal_or_distribution (false, (i, c)) (List.map (fun c -> (false, (i, c))) other_colors)
-  in let big_psy i = List.flatten (List.map (fun c -> phi i c) colors_set)
-  in List.flatten (List.map big_psy (list_to_indices seeds));;
+    clausal_distribution (false, (i, c)) (List.map (fun c -> (false, (i, c))) other_colors)
+  in let big_psi i = List.flatten (List.map (fun c -> psi i c) colors_set)
+  in List.flatten (List.map big_psi (list_to_indices seeds));;
 
-let clause_adjacence : Logic.literal list list = [];;
+(* ----------- Adjacence ----------- *)
+let adjacents_regions i seeds = [5];;
 
-let produce_contraints seeds adj : Logic.literal list list =
-  (clause_existence seeds @ clause_unicity seeds @ clause_adjacence);;
-  
+let clause_adjacence seeds : Logic.literal list list =
+  let psi i c = clausal_distribution 
+    (false, (i, c)) (List.map (fun v -> (false, (v, c))) (adjacents_regions i seeds))
+  in let big_psi i = List.flatten (List.map (fun c -> psi i c) colors_set)
+  in List.flatten (List.map big_psi (list_to_indices seeds));;
+
+let produce_constraints seeds adj : Logic.literal list list =
+  (clause_existence seeds @ clause_unicity seeds @ clause_adjacence seeds);;
+
+(* _________________________________________ 
+	    		RESOLUTION
+   _________________________________________ *)
+
+let rec extract_coloring results : Variables.t list =
+  match results with
+  | [] -> []
+  | (b, v)::rs ->
+    if b then
+      v :: (extract_coloring rs)
+    else
+      extract_coloring rs;;
+
+let generate_coloring seeds adj : Variables.t list option =
+  let solving = Sat.solve (produce_constraints seeds adj) in
+  match solving with
+  | None -> None
+  | Some results ->
+    let extraction = extract_coloring results in
+    match extraction with
+    | [] -> None
+    | _ -> Some extraction;;
