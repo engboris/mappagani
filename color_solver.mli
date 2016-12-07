536211f3904ca8c6d998576fed424147faf77f79
engboris <engboris@hotmail.fr>
2016-12-07 15:02:15 +0100

    . . .

--- color_solver.mli
+++ color_solver.mli
@@ -5,4 +5,4 @@ module Variables : sig
   val compare : t -> t -> int
 end
 
-val generate_coloring : (int * int -> int * int -> int) -> voronoi -> int array array -> Graphics.color list -> (int * Graphics.color) list
+val generate_coloring : (int * int -> int * int -> int) -> voronoi -> Graphics.color list -> int array array -> (int * Graphics.color) list
