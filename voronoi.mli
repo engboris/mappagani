ac68b779412abe21f8c756ab0bcf74795fc12ca5
KostiaChardonnet <kostia.chardonnet@yahoo.fr>
2016-12-07 13:29:22 +0100

    fix

--- voronoi.mli
+++ voronoi.mli
@@ -7,11 +7,9 @@ val indice_of_min : 'a array -> int
 val seed_of_pixel : 'a * 'b -> ('a * 'b -> int * int -> 'c) -> voronoi -> int
 val regions_voronoi : (int * int -> int * int -> 'a) -> voronoi -> int array array
 val adjacences_voronoi : voronoi -> int array array -> bool array array
-val adjacents_to : int -> bool array array -> int list 
+val adjacents_to : int -> bool array array -> int list
 val print_matrix : int array array -> unit
-(*val frontiere : 'a array array -> int -> int -> bool*)
 val getCouleur : Graphics.color option -> Graphics.color
-(*val draw_voronoi : int array array -> voronoi -> unit*)
 val get_list_couleurs : seed array -> Graphics.color list
 val fill_seeds : voronoi -> (int * Graphics.color) list -> voronoi
 val generator_color_set : voronoi -> Graphics.color list
