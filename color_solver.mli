154701e593f20edd16789d0f74300704e99d64aa
KostiaChardonnet <kostia.chardonnet@yahoo.fr>
2016-12-13 12:57:01 +0100

    no solution handling

--- color_solver.mli
+++ color_solver.mli
@@ -4,7 +4,8 @@ module Variables : sig
   type t
   val compare : t -> t -> int
 end
-
+ 
+exception NoSolution
 val seeds_to_indices : seed array -> int list
 val generate_coloring : (int * int -> int * int -> int) -> voronoi -> Graphics.color list -> int array array -> bool array array -> (int * Graphics.color) list
 val check_coloring : voronoi -> bool array array -> bool
