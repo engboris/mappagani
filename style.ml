a03fa75132cee4dbe0dd267ba4e457e50243679c
engboris <engboris@hotmail.fr>
2016-12-18 22:25:39 +0100

    Plus de couleurs

--- style.ml
+++ style.ml
@@ -1,13 +1,15 @@
-open Graphics;;
-
 (* _________________________________________
                   GENERAL
    _________________________________________ *)
 
+let black : color = 0x4b4848;;
 let blue : color = 0x94cbe3;;
 let red : color = 0xe88091;;
 let green : color = 0x9ded87;;
 let yellow : color = 0xf7fc8b;;
+let cyan : color = 0xc0ffff;;
+let magenta : color = 0xc975bb;;
+let white : color = 0x808080;;
 
 let background_color = 0x333536;;
 
@@ -16,10 +18,10 @@ let background_color = 0x333536;;
    _________________________________________ *)
 
 let button_background = 0x333536;;
-let button_textcolor = white;;
+let button_textcolor = 0xffffff;;
 let button_inactive_textcolor = 0x5a5a5a;;
 let button_bordercolor = 0x57cdff;;
-let button_hovercolor = 0x808080;;
+let button_hovercolor = white;;
 
 let default_width_menu_buttons = 200;;
 let default_height_menu_buttons = 30;;
