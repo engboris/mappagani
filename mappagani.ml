27301c0cac68db2a74fb230f3c97b8bf1c9f63a9
engboris <engboris@hotmail.fr>
2016-12-27 19:09:15 +0100

    Fonctionnalité retirée

--- mappagani.ml
+++ mappagani.ml
@@ -186,7 +186,7 @@ let rec game voronoi_main regions map_size menu screen_size state liste_pixel di
         check_buttons x_mouse y_mouse menu;
         if (coord_in_surface x_mouse y_mouse (0, 0) map_size) then
           let owner = regions.(x_mouse).(y_mouse) in
-          if (!newcolor <> None && original.seeds.(owner).c = None && !state <> End) then
+          if (!newcolor <> None && voronoi_main.seeds.(owner).c = None && !state <> End) then
   	        let seedtmp = {c=(!newcolor); x=voronoi_main.seeds.(owner).x; y=voronoi_main.seeds.(owner).y} in
   	        (voronoi_main.seeds.(owner) <- seedtmp;
             draw_regions regions voronoi_main liste_pixel owner;
